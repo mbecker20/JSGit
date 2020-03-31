e1bbedc1943d77cd775557033d20b1104b83375a
mbecker20 <becker.maxh@gmail.com>
2020-03-30 18:30:24 -0700

    added gui to index.html
    also moved stuff around in anim7 and commented out other anims in setup

--- Cycle/scripts/anim7.js
+++ Cycle/scripts/anim7.js
@@ -4,27 +4,53 @@ class Anim7 {
         this.node = BF.MakeTransformNode('anim4Node', scene);
 
         // setup lagrangian update system
-        function lFunc(p, pConst) {
-            var sphereI = pConst.sphereIcm + pConst.mSphere * MF.Square(p.l);
-            var cubeI = pConst.cubeIcm + pConst.mCube * MF.Square(pConst.lTot - p.l);
-            var t1 = .5 * (pConst.mSphere + pConst.mCube) * MF.Square(p.lDot);
-            var t2 = .5 * (sphereI * MF.Square(p.thetaDot) + cubeI * MF.Square(p.phiDot));
-            var t3 = pConst.mSphere * pConst.g * p.l * Math.cos(p.theta);
-            var t4 = pConst.mCube * pConst.g * (pConst.lTot - p.l) * Math.cos(p.phi);
-            return t1 + t2 + t3 + t4;
-        }
-
         this.dt = .01;
         this.stepsPerFrame = 2;
         this.params = {l: 6, lDot: 3, theta: 1, thetaDot: 2, phi: 0, phiDot: 10};
         this.pConst = {mSphere: 1, mCube: 1.1, g: 10, lTot: 10, rSphere: 1, sCube: 2};
+        this.damping = {lDot: .001, thetaDot: .001, phiDot: .001};
         this.pConst.sphereIcm = this.pConst.mSphere * (2/5) * MF.Square(this.pConst.rSphere);
         this.pConst.cubeIcm = this.pConst.mCube * (1/6) * MF.Square(this.pConst.sCube);
-        this.damping = {lDot: .001, thetaDot: .001, phiDot: .001};
-        this.lagrangian = new Lagrangian(lFunc, this.params, this.pConst, this.damping);
+
+        this.lagrangian = new Lagrangian(this.lFunc, this.params, this.pConst, this.damping);
         this.collisionVelocityMult = .6; // multiplied by -velocity on collision with pivot;
 
         // setup meshes
+        this.setupMeshs(scene);
+        
+        // set materials
+        this.ground.material = myMats.wArrow;
+        this.sphere.material = myMats.darkMoon;
+        this.cube.material = myMats.darkMoon;
+        this.spherePiv.material = myMats.darkMoon;
+        this.cubePiv.material = myMats.darkMoon;
+        this.topRope.material = myMats.wArrow;
+        this.sphereRope.material = myMats.wArrow;
+        this.cubeRope.material = myMats.wArrow;
+
+        // connect meshs to shadows and force pre-compile materials
+        BF.ConnectMeshsToShadows([this.ground, this.sphere, this.cube, this.spherePiv, this.cubePiv, this.topRope, this.sphereRope, this.cubeRope], shadows);
+        BF.ForceCompileMaterials([this.topRope, this.ground, this.sphere, this.cube, this.spherePiv, this.cubePiv]);
+
+        // set BC
+        this.lMin = this.spherePivR + this.pConst.rSphere;
+        this.lMax = this.pConst.lTot - this.cubePivR - this.pConst.sCube/2;
+
+        // set initial position of everything
+        this.setPos();
+    }
+
+    lFunc(p, pConst) {
+        var sphereI = pConst.sphereIcm + pConst.mSphere * MF.Square(p.l);
+        var cubeI = pConst.cubeIcm + pConst.mCube * MF.Square(pConst.lTot - p.l);
+        var t1 = .5 * (pConst.mSphere + pConst.mCube) * MF.Square(p.lDot);
+        var t2 = .5 * (sphereI * MF.Square(p.thetaDot) + cubeI * MF.Square(p.phiDot));
+        var t3 = pConst.mSphere * pConst.g * p.l * Math.cos(p.theta);
+        var t4 = pConst.mCube * pConst.g * (pConst.lTot - p.l) * Math.cos(p.phi);
+        return t1 + t2 + t3 + t4;
+    }
+
+    setupMeshs(scene) {
         this.ground = BABYLON.MeshBuilder.CreateGround('ground4', {width:10,height:10}, scene);
         this.ground.receiveShadows = true;
 
@@ -56,33 +82,12 @@ class Anim7 {
         this.cubeRope = BF.MakeTube('cubeRope', scene, .25);
         this.cubeRope.rotation.z = -Math.PI/2;
         
-        BF.BakeMeshs([this.cubeRope, this.sphereRope]); // have to set length with scaling.y;
+        BF.BakeMeshs([this.cubeRope, this.sphereRope]);
 
-        // setup Parenting
+        // setup parenting
         BF.SetChildren(this.node, [this.ground, this.spherePiv, this.cubePiv, this.topRope]);
         BF.SetChildren(this.spherePiv, [this.sphere, this.topRope, this.sphereRope]);
         BF.SetChildren(this.cubePiv, [this.cube, this.cubeRope]);
-
-        // set materials
-        this.ground.material = myMats.wArrow;
-        this.sphere.material = myMats.darkMoon;
-        this.cube.material = myMats.darkMoon;
-        this.spherePiv.material = myMats.darkMoon;
-        this.cubePiv.material = myMats.darkMoon;
-        this.topRope.material = myMats.wArrow;
-        this.sphereRope.material = myMats.wArrow;
-        this.cubeRope.material = myMats.wArrow;
-
-        // connect meshs to shadows and force pre-compile materials
-        BF.ConnectMeshsToShadows([this.ground, this.sphere, this.cube, this.spherePiv, this.cubePiv, this.topRope, this.sphereRope, this.cubeRope], shadows);
-        BF.ForceCompileMaterials([this.topRope, this.ground, this.sphere, this.cube, this.spherePiv, this.cubePiv]);
-
-        // set BC
-        this.lMin = this.spherePivR + this.pConst.rSphere;
-        this.lMax = this.pConst.lTot - this.cubePivR - this.pConst.sCube/2;
-
-        // set initial position of everything
-        this.setPos();
     }
 
     setPos() {
